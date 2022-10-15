fea5ef03977d76fae6e5f4d2fc0470e277717834
myshkovets <yulia_958@mail.ru>
2022-10-15 11:27:48 +0300

    first commit

---
+++ lesson10/src/test/java/ru/andersen/lesson10/AppTest.java
@@ -0,0 +1,20 @@
+package ru.andersen.lesson10;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
