b7c63a703dc803d7105c479f99eb1eb7e01a50b0
myshkovets <yulia_958@mail.ru>
2022-10-26 13:13:35 +0300

    first commit

---
+++ lesson13/src/test/java/org/example/AppTest.java
@@ -0,0 +1,53 @@
+package org.example;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ISelect;
+import org.openqa.selenium.support.ui.Select;
+
+
+class ContactUs {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\учеба\\Andersen JAVA\\Проекты\\дз 11.2\\lesson11\\src\\main\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+        driver.manage().window().maximize();
+
+        WebElement contactUsLink = driver.findElement(By.xpath("//*[@id=\"contact-link\"]/a"));
+        contactUsLink.click();
+
+        WebElement subjectHeading = driver.findElement(By.xpath("//*[@id=\"id_contact\"]"));
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+        WebElement message = driver.findElement(By.xpath("//*[@id=\"message\"]"));
+        WebElement sendButton = driver.findElement(By.xpath("//*[@id=\"submitMessage\"]/span"));
+
+        subjectHeading.click();
+        Select selectObject = new Select(subjectHeading);
+        selectObject.selectByValue("2");
+
+        email.click();
+        email.sendKeys("test@mail.ru");
+
+        message.click();
+        message.sendKeys("test");
+
+        sendButton.click();
+
+
+        WebElement successMessage = driver.findElement(By.xpath("//*[@id=\"center_column\"]/p"));
+        String actualMessage = successMessage.getText();
+        String expectedMessage = "Your message has been successfully sent to our team.";
+
+        Assert.assertEquals(actualMessage, expectedMessage);
+        System.out.println("test passed");
+
+        driver.close();
+
+
+    }
+}
