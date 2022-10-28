89977aea44c9ce24cb37d9c1866b54a8290037f3
myshkovets <yulia_958@mail.ru>
2022-10-28 16:49:26 +0300

    first commit

---
+++ lesson13/src/test/java/org/example/ForgotPassword.java
@@ -0,0 +1,46 @@
+package org.example;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Assert;
+
+
+class ForgotPassword {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+        driver.manage().window().maximize();
+
+        WebElement signInLink = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a"));
+
+        signInLink.click();
+
+        WebElement forgetPassword = driver.findElement(By.xpath("//*[@id='login_form']/div/p[1]/a"));
+
+        forgetPassword.click();
+        WebElement emailAddress = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+        emailAddress.click();
+        emailAddress.sendKeys("test@mail.ru");
+
+        WebElement retrievPasword = driver.findElement(By.xpath("//*[@id=\"form_forgotpassword\"]/fieldset/p/button/span"));
+        retrievPasword.click();
+
+
+        WebElement message = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/p"));
+        String actualMessage = message.getText();
+        String expectedMessage = "A confirmation email has been sent to your address: test@mail.ru";
+
+        Assert.assertEquals(actualMessage, expectedMessage);
+        System.out.println("test passed");
+
+        driver.close();
+
+    }
+
+}
