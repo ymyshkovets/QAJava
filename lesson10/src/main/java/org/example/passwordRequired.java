2651881c2f3a7a4653ee46ef2228aa6f3e823fea
myshkovets <yulia_958@mail.ru>
2022-10-28 14:26:31 +0300

    first commit

--- lesson10/src/main/java/org/example/passwordRequired.java
+++ lesson10/src/main/java/org/example/passwordRequired.java
@@ -9,7 +9,6 @@ import org.junit.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
-
 public class passwordRequired {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
